f9671a32959e24a4c1765084f5e272579d3e794d
lanuioe <lanuioe@gmail.com>
2024-01-02 19:39:35 +0900

    [Modify] main.js 코드 수정
    - Netlify 테스트를 위한 main.js 코드 수정

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import pb from '/src/api/pocketbase';
 // const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
-const test = await pb.collection('products').getList(1, 30);
-console.log(test);
+(async () => {
+  const test = await pb.collection('products').getList(1, 30);
+  console.log(test);
+})();
