9385ee4fac3ae1b02eced3b645aecbc17d2e7bb7
ParkjiDev <parkjidev@gmail.com>
2024-01-09 09:37:42 +0900

    [Chore] 상품 목록 페이지 구현을 위한 svg추가, 파일 연결
    - question 표시 svg 추가
    - page arrow svg 추가
    - tailwind 파일 연결

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         footer: resolve(__dirname, 'src/pages/components/footer.html'),
         popup: resolve(__dirname, 'src/pages/components/popup.html'),
         productBox: resolve(__dirname, 'src/pages/components/productBox.html'),
+        productList: resolve(__dirname, 'src/pages/productList/index.html'),
       },
     },
   },
