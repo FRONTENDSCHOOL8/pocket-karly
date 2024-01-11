38373f7486b39b71f0a91ffb5e1d5cc9b3e7eeac
HHeoGeon <gjrjs3333@naver.com>
2024-01-11 13:30:19 +0900

    [Feat] 로그인페이지 구현
    - db에 유저아이디가 있을 경우 로그인성공
    - 없을 경우 모달창 구현 필요
    - 로그인됐을 때 로컬스토리지에 저장

---
+++ src/pages/login/login.js
@@ -0,0 +1,31 @@
+import '/src/styles/tailwind.css';
+import pb from '/src/api/pocketbase';
+import { getNode, getStorage, setStorage } from '/src/lib/';
+
+const loginButton = getNode('.loginButton');
+
+async function handleLogin(e) {
+  e.preventDefault();
+
+  try {
+    const id = getNode('#userId').value;
+    const pw = getNode('#userPw').value;
+
+    await pb.collection('users').authWithPassword(id, pw);
+
+    const { model, token } = await getStorage('pocketbase_auth');
+
+    setStorage('auth', {
+      isAuth: !!model,
+      user: model,
+      token,
+    });
+
+    alert('로그인 완료! 메인페이지로 이동합니다.');
+    // window.location.href = '/index.html'
+  } catch {
+    alert('아이디,비밀번호를 확인해주세요.');
+  }
+}
+
+loginButton.addEventListener('click', handleLogin);
