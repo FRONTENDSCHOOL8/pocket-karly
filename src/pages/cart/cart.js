dd63effb6f0d6b1922f5886fae2ed728ecad22cd
lanuioe <lanuioe@gmail.com>
2024-01-12 21:34:42 +0900

    [Feat] 장바구니 기능 추가
    - 배송지 주소 API 추가
    - 상태변수 관리 추가 (checkState 함수 추가)
    - checkbox id 수정, data 속성 추가
    - 전체선택 checkbox 연결
    - 전체 상품 개수 추가

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -9,6 +9,7 @@ import {
 } from '/src/lib';
 import '/src/styles/tailwind.css';
 import '/src/pages/components/js/include.js';
+import { execDaumPostcode } from '/src/pages/components/js/addressApi.js';
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 let amount;
@@ -19,14 +20,19 @@ const productTemplate = getNode('.product--template');
 // 로그인한 유저의 id를 가져오도록 변경 필요
 const userData = await pb.collection('users').getOne('q4l7a4urcjb33hz');
 // const userData = await pb.collection('users').getOne('9vzsdelu39rzk6q');
-const { id } = userData;
+const { id, address } = userData;
+console.log('address: ', address);
 
 // 냉장식품 템플릿
 const cartDataCold = await pb.collection('carts_products_data').getFullList({
   filter: `packageType = "냉장식품" && users_record = "${id}"`,
   sort: 'created',
 });
+console.log('cartDataCold: ', cartDataCold);
 
+const coldState = {};
+
+// * 냉장식품 템플릿 반복문
 cartDataCold.forEach((cart) => {
   const { products_record, name, price, discount, amount, thumbImgAlt } = cart;
 
@@ -35,16 +41,18 @@ cartDataCold.forEach((cart) => {
   const totalPrice = price * amount;
 
   const templateProduct = /* html */ `
-    <li class="flex items-center px-1 py-5">
+    <li class="item flex items-center px-1 py-5">
       <label
-        for=${products_record}
+        for=${removeNumbers(products_record)}
         class="relative flex items-center text-l-base text-content"
       >
         <input
+          checked
           type="checkbox"
           name="select__product"
-          id=${products_record}
+          id=${removeNumbers(products_record)}
           class="peer absolute h-6 w-6 appearance-none"
+          data-record=${products_record}
         />
         <span
           aria-hidden="true"
@@ -118,13 +126,34 @@ cartDataCold.forEach((cart) => {
   `;
 
   insertFirst('.product__cold--template', templateProduct);
+
+  coldState[products_record] = true;
 });
+console.log('coldState 맨 처음:', coldState);
+
+function removeNumbers(value) {
+  const result = value.replace(/\d+/g, '');
+  return result;
+}
+
+// ^ checkState 함수
+// * target은 input(dom), products_record는 상품id
+function checkState(target, products_record) {
+  console.log('target,', target);
+  if (target.checked) {
+    coldState[products_record] = true;
+  } else {
+    coldState[products_record] = false;
+  }
+}
 
 // 냉동식품 템플릿
 const cartDataFreeze = await pb.collection('carts_products_data').getFullList({
   filter: `packageType = "냉동식품" && users_record = "${id}"`,
   sort: 'created',
 });
+
+const freezeState = {};
 
 cartDataFreeze.forEach((cart) => {
   const { products_record, name, price, discount, amount, thumbImgAlt } = cart;
@@ -140,10 +169,12 @@ cartDataFreeze.forEach((cart) => {
         class="relative flex items-center text-l-base text-content"
       >
         <input
+          checked
           type="checkbox"
           name="select__product"
-          id=${products_record}
+          id=${removeNumbers(products_record)}
           class="peer absolute h-6 w-6 appearance-none"
+          data-record=${products_record}
         />
         <span
           aria-hidden="true"
@@ -217,13 +248,19 @@ cartDataFreeze.forEach((cart) => {
   `;
 
   insertFirst('.product__freeze--template', templateProduct);
+
+  freezeState[products_record] = true;
 });
+
+console.log('freezeState 맨 처음:', freezeState);
 
 // 상온식품 템플릿
 const cartDataRoom = await pb.collection('carts_products_data').getFullList({
   filter: `packageType = "상온식품" && users_record = "${id}"`,
   sort: 'created',
 });
+
+const roomState = {};
 
 cartDataRoom.forEach((cart) => {
   const { products_record, name, price, discount, amount, thumbImgAlt } = cart;
@@ -239,10 +276,12 @@ cartDataRoom.forEach((cart) => {
         class="relative flex items-center text-l-base text-content"
       >
         <input
+          checked
           type="checkbox"
           name="select__product"
-          id=${products_record}
+          id=${removeNumbers(products_record)}
           class="peer absolute h-6 w-6 appearance-none"
+          data-record=${products_record}
         />
         <span
           aria-hidden="true"
@@ -316,8 +355,75 @@ cartDataRoom.forEach((cart) => {
   `;
 
   insertFirst('.product__room--template', templateProduct);
+
+  roomState[products_record] = true;
 });
 
+console.log('roomState 맨 처음:', roomState);
+
+/* -------------------------------------------------------------------------- */
+// 상품 개수
+const productCountElem = getNodes('.product__count');
+
+// 전체 상품 개수
+const productTotalNum =
+  cartDataCold.length + cartDataFreeze.length + cartDataRoom.length;
+console.log('productTotalNum: ', productTotalNum);
+
+// 선택 상품 개수 - 임시로 설정
+const productSelectedNum = 2;
+
+[...productCountElem].forEach((elem) => {
+  elem.textContent = `(${productSelectedNum}/${productTotalNum})`;
+});
+
+const addressElem = getNode('address');
+addressElem.textContent = address;
+
+/* -------------------------------------------------------------------------- */
+// * 회원가입,장바구니 둘 다 쓸 수 있는 함수로 리팩토링 하면 좋을 듯
+const selectAlls = getNodes('input[name="select-all"]');
+
+export const checkAll = (elem) => {
+  return (e) => {
+    const { target } = e;
+
+    const select = getNodes(elem);
+    // console.log('select: ', select);
+    if (target.checked) {
+      select.forEach((item) => {
+        item.checked = true;
+      });
+    } else {
+      select.forEach((item) => {
+        item.checked = false;
+      });
+    }
+  };
+};
+
+// 전체선택 연결
+function connectSelect(elem) {
+  const [checkAll1, checkAll2] = elem;
+  checkAll2.addEventListener('change', function () {
+    checkAll1.checked = this.checked;
+  });
+
+  checkAll1.addEventListener('change', function () {
+    checkAll2.checked = this.checked;
+  });
+}
+
+// 전체선택 시 모든 checkbox 선택
+selectAlls.forEach((selectAll) => {
+  connectSelect(selectAlls);
+  selectAll.addEventListener(
+    'click',
+    checkAll('input[name="select__product"]')
+  );
+});
+
+/* -------------------------------------------------------------------------- */
 // 수량 변경 함수
 function changeAmount(elemTarget, eventTarget) {
   const beforeAmount = Number(elemTarget.innerText);
@@ -396,7 +502,7 @@ function calcTotalPrice() {
   const realPriceAcc = arrayToSum(realPriceArray);
   const discountPriceAcc = arrayToSum(discountPriceArray);
 
-  console.log(realPriceAcc, discountPriceAcc);
+  // console.log(realPriceAcc, discountPriceAcc);
   return { realPriceAcc, discountPriceAcc };
 }
 
@@ -442,27 +548,73 @@ function updateTemplate() {
 }
 updateTemplate();
 
+/* -------------------------------------------------------------------------- */
 // 버튼 클릭 이벤트 함수
 function handleButton(e) {
   const button = e.target.closest('button');
-  if (!button) {
+  const input = e.target.closest('input');
+
+  if (!button && !input) {
     return;
   }
-  const minusButton = button.classList.contains('button__minus');
-  const plusButton = button.classList.contains('button__plus');
-  if (minusButton || plusButton) {
-    const amountElem =
-      button.previousElementSibling || button.nextElementSibling;
-    changeAmount(amountElem, plusButton);
-    console.log('수량 버튼 클릭');
-    calcTotalPrice();
-    updateTemplate();
-    return;
+  if (button) {
+    const minusButton = button.classList.contains('button__minus');
+    const plusButton = button.classList.contains('button__plus');
+    if (minusButton || plusButton) {
+      const amountElem =
+        button.previousElementSibling || button.nextElementSibling;
+      changeAmount(amountElem, plusButton);
+      console.log('수량 버튼 클릭');
+      calcTotalPrice();
+      updateTemplate();
+      return;
+    } else {
+      console.log('삭제 버튼 클릭');
+      deleteProduct(e);
+      calcTotalPrice();
+      updateTemplate();
+    }
   }
-  console.log('삭제 버튼 클릭');
-  deleteProduct(e);
-  calcTotalPrice();
-  updateTemplate();
+  if (input) {
+    const productId = input.dataset.record;
+    console.log('productId: ', productId);
+    checkState(input, productId);
+    console.log('coldState:', coldState);
+    console.log('freezeState:', freezeState);
+    console.log('roomState:', roomState);
+
+    // coldState의 value를 순회해서 true값인 key만 찾기
+  }
 }
+
+// ^value로 key찾기
+// const trueKeys = Object.keys(sampleObject).filter(key => sampleObject[key] === true);
+
+// function trueKeys(obj) {
+//   return Object.keys(obj).reduce((acc, key) => {
+//     if (obj[key] === true) {
+//       acc.push(key);
+//     }
+//     return acc;
+//   }, []);
+// }
+
+const selectDeleteButton = getNodes('.button--delete__select');
+[...selectDeleteButton].forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log('선택 삭제 버튼 클릭');
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+// 배송지 변경 함수
+const changeAddressButton = getNode('.button__change-address');
+
+changeAddressButton.addEventListener('click', () => {
+  console.log('배송지 변경 버튼 클릭');
+});
+changeAddressButton.addEventListener('click', () => {
+  execDaumPostcode();
+});
 
 productTemplate.addEventListener('click', handleButton);
